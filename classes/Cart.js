3405ce50516fb8d7bf127699b4e0e4b68f58b6cd
JLKennard <jessicakennard@hotmail.co.uk>
2023-04-24 19:42:46 +0100

    updated class js

---
+++ classes/Cart.js
@@ -0,0 +1,17 @@
+class Cart {
+  constructor() {
+    this.products = [];
+    this.total = 0;
+  }
+  addProduct(product) {
+    this.products.push(product);
+    this.total += product.price;
+  }
+  removeProduct(i) {
+    const product = this.products[i];
+    this.products.splice(i, 1);
+    this.total -= product.price;
+  }
+}
+
+module.exports = Cart;
